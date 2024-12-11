ed782b2184d66c5a9130a6f0aebdbdbb2c29c33c
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-12-11 13:52:23 -0300

    first commit

---
+++ wp_precnet_api_consumer.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+ * Plugin Name: Precnet Api Consumer
+ * Description: Plugin to send elementor form to API
+ * Plugin URI:  https://github.com/lucassdantas/wp_precnet_api_consumer.git
+ * Version:     1.0.0
+ * Author:      RD Exclusive
+ * Author URI:  https://rdmarketing.com.br/
+ */
